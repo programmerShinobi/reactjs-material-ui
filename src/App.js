d9be0bd99d624cc04299e7bf409dc433cd5ee50b
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-01-23 22:01:03 +0700

    Update module FAQ

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices/_index";
 import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
-// import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 // import Bar from "./scenes/bar";
 // import Pie from "./scenes/pie";
 // import Line from "./scenes/line";
@@ -35,10 +35,10 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
