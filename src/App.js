5dc82901ced881fb5b4d0851a637ba6137fa3a94
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-01-23 17:24:54 +0700

    Update module User Profile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team/_index";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices/_index";
-// import Form from "./scenes/form";
+import Form from "./scenes/form";
 // import Calendar from "./scenes/calendar/calendar";
 // import FAQ from "./scenes/faq";
 // import Bar from "./scenes/bar";
@@ -33,7 +33,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
